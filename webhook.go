83f7c32be2af842a2fa670b0f6e47d6a666808dc
Ethan C <cavesystems11@gmail.com>
2020-08-08 12:48:52 -0700

    Webhook override

--- webhook.go
+++ webhook.go
@@ -9,10 +9,16 @@ import (
 
 type WebhookMessage struct {
 	Content string `json:"content"`
+	Username string `json:"username"`
+	AvatarUrl string `json:"avatar_url"`
 }
 
 func SendToWebhook(str string) error {
-	m, _ := json.Marshal(WebhookMessage{Content: str})
+	m, _ := json.Marshal(WebhookMessage{
+		Content: str,
+		Username: "Pixels Storage Engine",
+		AvatarUrl: "https://i.imgur.com/3jyIODH.png",
+	})
 	res, err := http.Post(os.Getenv(DiscordWebhookURL), "application/json", bytes.NewBuffer(m))
 	if err != nil {
 		return err
